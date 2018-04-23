bdda26bbca2ec70f1acbc0e2ba921014b08fac9d
Bankworthy <aidan.cadogan@hotmail.co.uk>
2018-04-23 20:04:17 +0100

    door closing anim (one side only)

---
+++ UE4Source/ProjectRedhouse/Source/ProjectRedhouse/MyAssetUserData.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetUserData.h"
+#include "MyAssetUserData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREDHOUSE_API UMyAssetUserData : public UAssetUserData
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
